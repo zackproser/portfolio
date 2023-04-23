4fbfc83bd4dacee1f825c0a456dc862a5663159e
Zachary Proser <zackproser@gmail.com>
2023-04-22 23:51:55 -0400

    Implement initial about page copy

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -12,6 +12,10 @@ import {
 } from '@/components/SocialIcons'
 import portraitImage from '@/images/zachary-proser.png'
 
+function RenderNumYearsExperience() {
+  return Math.abs(new Date('January 1 2012').getFullYear() - new Date().getFullYear())
+}
+
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
     <li className={clsx(className, 'flex')}>
@@ -65,30 +69,30 @@ export default function About() {
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                I'm a tech lead and senior software engineer at Gruntwork.io, a startup 
+                that specializes in infrastructure as code, AWS, DevOps and automation. 
+                I love to build, maintain and enhance useful tools, especially in Golang
+                using the TUI library bubbletea from Charm.sh. 
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                I've been a software developer and open-source contributor for {RenderNumYearsExperience()} years. 
+                All of my open-source work lives at <a href="https://github.com/zackproser">https://github.com/zackproser</a>
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                In my free time, I love to research new techniques and to practice keyboard-driven development using tmux, neovim, awesome 
+                window manager and other powerful open source tools. 
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                I believe deeply in sharing knowledge and taking the time out to pass along what you know to others, just as so many developers 
+                have done for me. 
+              </p>
+              <p>
+                This site is a part of the way I give back and say thanks to those who took time out of their day to stop what they were doing 
+                and show me a better way to accomplish a task, to use a tool, to decompose a problem. 
+              </p>
+              <p>
+                If you find shells, automation, security hardening your system and workflow, high performance computing and automating complex 
+                deployments with custom state machines and cloud infrastructure, then you&apos;ll probably enjoy my content. 
               </p>
             </div>
           </div>
