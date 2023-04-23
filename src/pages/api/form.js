a75267a936e66ebe89d7bfe7c4e5df23bcdfca8c
Zachary Proser <zackproser@gmail.com>
2023-04-22 20:51:53 -0400

    Get custom backend working for subscription form

--- src/pages/api/form.js
+++ src/pages/api/form.js
@@ -1,14 +1,43 @@
+import md5 from 'md5'
+
 export default function handler(req, res) {
   // Get data submitted in request's body.
   const body = req.body
 
-  // Optional logging to see the responses
-  // in the command line where next.js app is running.
-  console.log('email: ', body.email)
+  // if email is missing, return an error 
+  if (body.email == "") {
+    res.status(400).json({ data: `Error: no valid email found in request`})
+  }
 
-  // Guard clause checks for first and last name,
+  const emailOctopusAPIKey = process.env.EMAIL_OCTOPUS_API_KEY
+  const emailOctopusListId = process.env.EMAIL_OCTOPUS_LIST_ID
+  const newMemberEmailAddress = body.email
+  const emailOctopusAPIEndpoint = `https://emailoctopus.com/api/1.6/lists/${emailOctopusListId}/contacts`
 
-  // Found the name.
+  console.dir({
+    key: emailOctopusAPIKey, 
+    listId: emailOctopusListId,
+    newMemberEmailAddress, 
+    emailOctopusAPIEndpoint, 
+  })
+
+  const data = {
+    api_key: emailOctopusAPIKey, 
+    email_address: newMemberEmailAddress,
+    status: "SUBSCRIBED"
+  } 
+
+  const requestOptions = {
+    crossDomain: true, 
+    method: 'POST', 
+    headers: {'Content-type':'application/json'}, 
+    body: JSON.stringify(data)
+  }
+
+  fetch(emailOctopusAPIEndpoint, requestOptions)
+    .then(response => response.json())
+    .then(data => console.dir(data))
+
   // Sends a HTTP success code
-  res.status(200).json({ data: `${body.email}` })
+  res.status(200).json({ data: `Think we successfully subscribed ${body.email}` })
 }
