ddd37ad4d8d4fc14bbcdbab50f3f622a70c1e3a2
Zachary Proser <zackproser@gmail.com>
2023-04-24 23:04:18 +0000

    Fix RSS feed 404s

--- src/components/ArticleLayout.jsx
+++ src/components/ArticleLayout.jsx
@@ -66,7 +66,7 @@ export function ArticleLayout({
   return (
     <>
       <Head>
-        <title>{`${meta.title} - Spencer Sharp`}</title>
+        <title>{`${meta.title} - Zachary Proser`}</title>
         <meta name="description" content={meta.description} />
       </Head>
       <Container className="mt-16 lg:mt-32">
