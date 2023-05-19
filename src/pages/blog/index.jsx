6440fca7a9a6f961118ba0682774efe3d89ffca7
Zachary Proser <zackproser@gmail.com>
2023-05-19 00:54:32 -0400

    feat(blog): add featured images to articles
    Added featured images to all articles, which will be displayed on the blog index and social media shares. Updated the `getAllArticles` function to include the `image` property in the article metadata. Also updated the `BlogPostCard` component to display the featured image.

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -4,54 +4,42 @@ import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
 import { formatDate } from '@/lib/formatDate'
 import { getAllArticles } from '@/lib/getAllArticles'
-import wakkaImg from '@/images/wakka.png'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import wakka from '@/images/wakka.png'
 
 function BlogPostCard({ article }) {
   return (
     <article key={article.slug} className="flex flex-col items-start justify-between">
       <div className="relative w-full">
-        <img
-          src={article.image ?? wakkaImg}
-          alt={article.slug}
-          className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-        />
+        <Link href={`/blog/${article.slug}`}>
+          <Image
+            src={article.image ?? wakka}
+            alt={article.slug}
+            className="aspect-[16/9] w-full rounded-2xl bg-gray-50 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+          />
+        </Link>
         <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
       </div>
       <div className="max-w-xl">
-        <div className="mt-8 flex items-center gap-x-4 text-xs">
-          <time className="text-gray-500">
+        <div className="flex items-center gap-x-4 text-xs">
+          <time className="text-gray-50">
             {article.date}
           </time>
-          <a
-            href={article.href}
-            className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-          >
-            {article.title}
-          </a>
         </div>
         <div className="group relative">
-          <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-            <a href={article.href}>
-              <span className="absolute inset-0" />
+          <h3 className="mt-3 text-lg font-semibold leading-6 text-zinc-800 dark:text-zinc-100 group-hover:text-gray-600">
+            <Link href={`/blog/${article.slug}`} >
+              <span className="absolute inset-0 " />
               {article.title}
-            </a>
+            </Link>
           </h3>
-          <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{article.description}</p>
-        </div>
-        <div className="relative mt-8 flex items-center gap-x-4">
-          <img src={article.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-100" />
-          <div className="text-sm leading-6">
-            <p className="font-semibold text-gray-900">
-              <a href={article.author.href}>
-                <span className="absolute inset-0" />
-                {article.author}
-              </a>
-            </p>
-            <p className="text-gray-600">Grand PooBah</p>
-          </div>
+          <Link href={`/blog/${article.slug}`}><p className="mt-5 line-clamp-3 text-sm leading-6 text-zinc-800 dark:text-zinc-400 ">{article.description}</p></Link>
         </div>
       </div>
-    </article>
+    </article >
   )
 }
 
