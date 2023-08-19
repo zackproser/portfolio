2bbd18711879cda19a271136f94ad14cd361ba36
Zachary Proser <zackproser@gmail.com>
2023-08-19 13:48:35 -0400

    Fix video page titles

--- src/pages/videos/index.jsx
+++ src/pages/videos/index.jsx
@@ -12,12 +12,12 @@ export default function VideosIndex({ videos }) {
         <title>Videos - Zachary Proser</title>
         <meta
           name="description"
-          content="All of my technical videos, Twitch live streams, demos, and code walkthroughs"
+          content="All of my latest Twitch streams, YouTube videos, how-tos, demos, and code walkthroughs"
         />
       </Head>
       <SimpleLayout
-        title="I write to learn and discover, and I publish to share knowledge."
-        intro="All of my technical tutorials, deep-dives, developer rants and video walkthroughs"
+        title="Zachary Proser's Videos"
+        intro="All of my latest Twitch streams, YouTube videos, how-tos, demos, and code walkthroughs"
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
