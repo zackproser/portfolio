713f4257cf1e0def8094ea4e33ca0563e730b1f7
Zachary Proser <zackproser@gmail.com>
2023-08-19 13:30:16 -0400

    Split out articles and videos - we're growing baby

--- src/pages/videos/index.jsx
+++ src/pages/videos/index.jsx
@@ -1,18 +1,18 @@
 import Head from 'next/head'
 
 import { SimpleLayout } from '@/components/SimpleLayout'
-import { getAllArticles } from '@/lib/getAllArticles'
+import { getAllVideos } from '@/lib/getAllVideos'
 
 import { BlogPostCard } from '@/components/BlogPostCard'
 
-export default function ArticlesIndex({ articles }) {
+export default function VideosIndex({ videos }) {
   return (
     <>
       <Head>
-        <title>Articles - Zachary Proser</title>
+        <title>Videos - Zachary Proser</title>
         <meta
           name="description"
-          content="All of my technical tutorials, deep-dives, developer rants and video walkthroughs"
+          content="All of my technical videos, Twitch live streams, demos, and code walkthroughs"
         />
       </Head>
       <SimpleLayout
@@ -21,8 +21,8 @@ export default function ArticlesIndex({ articles }) {
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {articles.map((article) => (
-              <BlogPostCard key={article.slug} article={article} />
+            {videos.map((video) => (
+              <BlogPostCard key={video.slug} article={video} />
             ))}
           </div>
         </div>
@@ -34,7 +34,12 @@ export default function ArticlesIndex({ articles }) {
 export async function getStaticProps() {
   return {
     props: {
-      articles: (await getAllArticles()).map(({ component, ...meta }) => meta),
+      videos: (await getAllVideos()).map(({ component, ...meta }) => ({
+        ...meta,
+        type: 'video',
+      })),
     },
   }
 }
+
+
