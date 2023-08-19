e68ec35983f235f1bbb7b2e90b4c94dd8293ce92
Zachary Proser <zackproser@gmail.com>
2023-08-19 11:42:59 -0400

    Absolute brutality

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 
-import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import { formatDate } from '@/lib/formatDate'
 import { getAllArticles } from '@/lib/getAllArticles'
 
 import Image from 'next/image'
@@ -40,35 +38,6 @@ function BlogPostCard({ article }) {
         </div>
       </div>
     </article >
-  )
-}
-
-function Article({ article }) {
-  return (
-    <article className="md:grid md:grid-cols-4 md:items-baseline">
-      <Card className="md:col-span-3">
-        <Card.Title href={`/blog/${article.slug}`}>
-          {article.title}
-        </Card.Title>
-        <Card.Eyebrow
-          as="time"
-          dateTime={article.date}
-          className="md:hidden"
-          decorate
-        >
-          {formatDate(article.date)}
-        </Card.Eyebrow>
-        <Card.Description>{article.description}</Card.Description>
-        <Card.Cta>Read article</Card.Cta>
-      </Card>
-      <Card.Eyebrow
-        as="time"
-        dateTime={article.date}
-        className="mt-1 hidden md:block"
-      >
-        {formatDate(article.date)}
-      </Card.Eyebrow>
-    </article>
   )
 }
 
