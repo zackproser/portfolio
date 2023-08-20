dd4c115e090f4d115625771ed35d2f7a8467c967
Zachary Proser <zackproser@gmail.com>
2023-08-20 10:42:32 -0400

    Check in latest

--- src/pages/api/og.jsx
+++ src/pages/api/og.jsx
@@ -6,9 +6,35 @@ export const config = {
 
 export default async function handler(request) {
   const { searchParams } = new URL(request.url);
-  const username = 'zackproser'
+  console.log(`og API route searchParams %o:`, searchParams)
   const hasTitle = searchParams.has('title');
-  const title = hasTitle ? searchParams.get('title') : 'Staff Developer Advocate at Pinecone.io'
+  const title = hasTitle ? searchParams.get('title') : 'Staff Developer Advocate at Pinecone.io';
+  const hasImage = searchParams.has('amp;image');
+  const image = hasImage ? searchParams.get('amp;image') : undefined;
+
+  console.log(`og API route hasImage: ${hasImage}, image: ${image}`)
+
+  const profileImageFetchURL = new URL('/public/zack.png', import.meta.url);
+
+  console.log(`og API route profileImageFetchURL: ${profileImageFetchURL}`)
+
+  const profileImageData = await fetch(profileImageFetchURL).then(
+    (res) => res.arrayBuffer(),
+  );
+
+  const fetchURL = () => {
+    setTimeout(() => {
+      return new URL(image, import.meta.url);
+    }, 300)
+  }
+
+  console.log(`og API route fetchURL: ${fetchURL}`)
+
+  const postImageData = await fetch(new URL(fetchURL, import.meta.url)).then(
+    (res) => res.arrayBuffer(),
+  ).catch((err) => {
+    console.log(`og API route err: ${err}`);
+  });
 
   return new ImageResponse(
     (
@@ -31,13 +57,23 @@ export default async function handler(request) {
             color: '#00000'
           }}>Zachary Proser</p>
         <img
-          width="256"
-          height="256"
-          src={`https://github.com/${username}.png`}
+          width="156"
+          height="156"
+          src={profileImageData}
           style={{
             borderRadius: 128,
           }}
         />
+        {hasImage &&
+          <img
+            width="56"
+            height="56"
+            src={postImageData}
+            style={{
+              borderRadius: 128,
+            }}
+          />
+        }
         <p
           style={{
             color: '#00000',
