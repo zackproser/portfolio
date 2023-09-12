778d430a08bddb4e7c20c33dc04528be497aa3ed
Zachary Proser <zackproser@gmail.com>
2023-09-11 23:24:24 -0400

    Various copy tweaks and fix for article date format

--- src/components/ArticleLayout.jsx
+++ src/components/ArticleLayout.jsx
@@ -5,7 +5,6 @@ import { Container } from '@/components/Container'
 import { Newsletter } from '@/components/Newsletter'
 import FollowButtons from '@/components/FollowButtons'
 import { Prose } from '@/components/Prose'
-import { formatDate } from '@/lib/formatDate'
 
 function ArrowLeftIcon(props) {
   return (
@@ -96,7 +95,7 @@ export function ArticleLayout({
                   className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
                 >
                   <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
-                  <span className="ml-3">{formatDate(meta.date)}</span>
+                  <span className="ml-3">{meta.date}</span>
                 </time>
               </header>
               <Prose className="mt-8">{children}</Prose>
