785a9a6c01b25906f14ae855b5b6be793073d559
Zachary Proser <zackproser@gmail.com>
2023-10-07 00:22:54 -0400

    Fix image and button clickability

--- src/components/ConsultingCaptureModal.jsx
+++ src/components/ConsultingCaptureModal.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
+import { Button } from '@/components/Button';
 
 
 import logoPinecone from '@/images/logos/pinecone-logo.png'
@@ -59,33 +60,9 @@ export default function ConsultingCaptureModal({ delay }) {
           <div className="w-full md:w-1/2 bg-slate-900 hidden md:block">
             <Image
               src={hackerForHire}
-              className="h-full w-100"
+              className="w-100"
               alt="Hacker for hire"
             />
-            <svg
-              viewBox="0 0 926 676"
-              aria-hidden="true"
-              className="absolute -bottom-24 left-24 w-[57.875rem] transform-gpu blur-[118px]"
-            >
-              <path
-                fill="url(#60c3c621-93e0-4a09-a0e6-4c228a0116d8)"
-                fillOpacity=".4"
-                d="m254.325 516.708-90.89 158.331L0 436.427l254.325 80.281 163.691-285.15c1.048 131.759 36.144 345.144 168.149 144.613C751.171 125.508 707.17-93.823 826.603 41.15c95.546 107.978 104.766 294.048 97.432 373.585L685.481 297.694l16.974 360.474-448.13-141.46Z"
-              />
-              <defs>
-                <linearGradient
-                  id="60c3c621-93e0-4a09-a0e6-4c228a0116d8"
-                  x1="926.392"
-                  x2="-109.635"
-                  y1=".176"
-                  y2="321.024"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stopColor="#776FFF" />
-                  <stop offset={1} stopColor="#FF4694" />
-                </linearGradient>
-              </defs>
-            </svg>
           </div>
           <div className="w-full md:w-2/3 pl-6 pr-6">
             <p className="text-3xl font-bold tracking-tight text-indigo-500 sm:text-4xl">Hacker for hire!</p>
@@ -103,13 +80,13 @@ export default function ConsultingCaptureModal({ delay }) {
               team dynamics and improving velocity. I can provide product feedback, hands-on stress testing and marketing amplification, and a staff-level developer
               perspective on your product or strategy.
             </p>
-            <div className="mt-6 mb-3">
-              <Link
-                href="https://calendly.com/zackproser/60min"
-                className="inline-flex rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-white/20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            <div className="ml-22 p-8 mt-6 mb-3 z-50">
+              <Button
+                className="bg-indigo-400 dark:bg-indigo-600 hover:bg-indigo-500"
+                href={"https://calendly.com/zackproser/60min"}
               >
-                Book a call with me - $500 per hour
-              </Link>
+                Book a consultation - $500 per hour
+              </Button>
             </div>
           </div>
         </div>
