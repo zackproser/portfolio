24bf9197836519112c848714c7b93db3570c5eb1
Zachary Proser <zackproser@gmail.com>
2023-10-07 09:36:05 -0400

    Tweak about page - add 2 CTAs

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,8 @@ const variantStyles = {
     'bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70',
   green:
     'bg-green-400 text-xl font-extrabold text-white hover:bg-green-400 active:bg-green-400 active:text-zinc-900/60 dark:bg-green-400/50 dark:text-zinc-300 dark:hover:bg-green-400 dark:hover:text-zinc-50 dark:active:bg-green-400/50 dark:active:text-zinc-50/70',
-
+  blue:
+    'bg-indigo-700 dark:bg-indigo-700 text-xl font-extrabold text-white hover:bg-indigo-400 active:bg-indigo-400 active:text-zinc-900/60 dark:bg-indigo-400/50 dark:text-zinc-300 dark:hover:bg-indigo-400 dark:hover:text-zinc-50 dark:active:bg-indigo-300/50 dark:active:text-zinc-50/70'
 }
 
 export function Button({ variant = 'primary', className, href, ...props }) {
