5ec2310af6e701b36ead5656a0a10ef50126e980
Zachary Proser <zackproser@gmail.com>
2023-10-09 20:40:48 -0400

    Update blog header and tagline

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -16,8 +16,8 @@ export default function ArticlesIndex({ articles }) {
         />
       </Head>
       <SimpleLayout
-        title="I write to learn and discover, and I publish to share knowledge."
-        intro="All of my technical tutorials, deep-dives, developer rants and video walkthroughs"
+        title="I write to learn. I publish to share."
+        intro="All of my technical tutorials, musings and developer rants"
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
