098a217a0813a74e3f9c8f97c98bdb046365dd6e
Zachary Proser <zackproser@gmail.com>
2023-10-28 10:29:18 -0400

    Progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,12 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Spencer Sharp',
+    template: '%s - Zachary Proser',
     default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+      'Zachary Proser - Open-source hacker, writer, and life-long learner',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I’m Zachary, a staff developer advocate at Pinecone.io where we build a high-scale vector database which is critcal infrastructure for the AI-boom',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
