916658402ac0203665ec1dab088827167c009dae
Zachary Proser <zackproser@gmail.com>
2023-10-28 23:42:29 -0400

    Escape apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Let's level up!
+            Let&apos;s level up!
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I acquire and share knowledge. I <Link href={"https://github.com/zackproser"} className="text-green-500 font-extrabold"> open-source </Link>the majority of my work. You can <Link href={"/about"} className="text-green-500 font-extrabold"> read more about me here</Link>.
