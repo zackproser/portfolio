019fa7ff04cd9a7b8b60a459c7373f008118c8a8
Zachary Proser <zackproser@gmail.com>
2023-10-28 23:31:54 -0400

    Let's fuck with everything at the same time

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -107,7 +107,7 @@ export const Newsletter = function() {
             required
             className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
           />
-          <Button type="submit" className="ml-4 flex-none">
+          <Button variant="green" type="submit" className="ml-4 flex-none">
             Count me in
           </Button>
         </div>
