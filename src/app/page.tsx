678abd24c9030b8d95af727f7f016519371a1fb8
Zachary Proser <zackproser@gmail.com>
2023-10-28 23:31:54 -0400

    Let's fuck with everything at the same time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,10 +72,10 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Open-source hacker and technical writer
+            Let's level up!
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I acquire and share knowledge. I constantly experiment, create tutorials,  demo videos and open-source repos to share my best discoveries. I <Link href={"https://github.com/zackproser"} className="text-green-500 font-extrabold"> open-source </Link>the majority of my work. You can <Link href={"/about"} className="text-green-500 font-extrabold"> read more about me here</Link>.
+            I acquire and share knowledge. I <Link href={"https://github.com/zackproser"} className="text-green-500 font-extrabold"> open-source </Link>the majority of my work. You can <Link href={"/about"} className="text-green-500 font-extrabold"> read more about me here</Link>.
           </p>
           <Newsletter />
           <div className="mt-6 flex gap-6">
