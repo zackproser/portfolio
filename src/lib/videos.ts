c352d43c480aded24273f850f2aa4078ca485c33
Zachary Proser <zackproser@gmail.com>
2023-10-28 21:35:36 -0400

    MIGRATE ALL THE VIDEOS

--- src/lib/videos.ts
+++ src/lib/videos.ts
@@ -1,23 +1,25 @@
 import { Article, ArticleWithSlug } from './shared-types'
+
 import glob from 'fast-glob'
 
 async function importArticle(
   articleFilename: string,
 ): Promise<ArticleWithSlug> {
-  let { article } = (await import(`../app/blog/${articleFilename}`)) as {
+  let { article } = (await import(`../app/videos/${articleFilename}`)) as {
     default: React.ComponentType
     article: Article
   }
 
   return {
     slug: articleFilename.replace(/(\/page)?\.mdx$/, ''),
+    type: 'video',
     ...article,
   }
 }
 
-export async function getAllArticles() {
+export async function getAllVideos() {
   let articleFilenames = await glob('*/page.mdx', {
-    cwd: './src/app/blog',
+    cwd: './src/app/videos',
   })
 
   let articles = await Promise.all(articleFilenames.map(importArticle))
