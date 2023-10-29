4226c2c801cd8811b5de5e6aa24f918f4857401d
Zachary Proser <zackproser@gmail.com>
2023-10-29 09:25:46 -0400

    Fix 404 page on mobile

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -6,15 +6,18 @@ import wizard404 from '@/images/404-wizard.jpeg';
 
 export default function Custom404() {
   return (
-    <Container className="mt-16 sm:mt-32">
-      <h1 className="m-12 leading-7 dark:text-zinc-400 text-9xl text-center">404</h1>
-      <h2 className="m-12 leading-3 dark:text-zinc-400 text-3xl text-center">Whoops - we couldn&apos;t find that page!</h2>
-      <Image src={wizard404} alt="404 wizard" />
-      <Link href={'/'} className="m-12 leading-3 dark:text-zinc-400 text-3xl text-center"
-      >
-        <h1 className="m-12 leading-7 dark:text-zinc-400 text-9xl text-center">Go Home?</h1>
+    <Container className="mt-16 lg:mt-32">
+      <h1 className="mb-8 text-4xl sm:text-7xl lg:text-9xl text-center leading-none dark:text-zinc-400">404</h1>
+      <h2 className="mb-8 text-xl sm:text-2xl lg:text-3xl text-center leading-tight dark:text-zinc-400">
+        Whoops - we couldn&apos;t find that page!
+      </h2>
+      <div className="w-full">
+        <Image src={wizard404} alt="404 wizard" layout="responsive" />
+      </div>
+      <Link href="/" className="block mt-8 mb-4 text-xl sm:text-2xl lg:text-3xl text-center leading-tight dark:text-zinc-400">
+        Go Home?
       </Link>
-
     </Container>
   );
 }
+
