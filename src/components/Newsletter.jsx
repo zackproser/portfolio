8783b0abe374b612a33e9dcb606271e5be898fa1
Zachary Proser <zackproser@gmail.com>
2023-10-28 23:38:25 -0400

    Facelift Newsletter form

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -8,7 +8,7 @@ function MailIcon(props) {
   return (
     <svg
       viewBox="0 0 24 24"
-      fill="none"
+      fill="#21fc0d"
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
@@ -17,11 +17,11 @@ function MailIcon(props) {
     >
       <path
         d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
+        className="fill-green-100 stroke-green-400 dark:fill-green-100/10 dark:stroke-green-500"
       />
       <path
         d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
+        className="stroke-green-400 dark:stroke-green-500"
       />
     </svg>
   )
@@ -93,10 +93,10 @@ export const Newsletter = function() {
 
         <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
           <MailIcon className="h-6 w-6 flex-none" />
-          <span className="ml-3">Supercharge your development skills</span>
+          <span className="ml-3">Supercharge your development skills ⚡</span>
         </h2>
         <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-          I publish deep-dive technical content for professional developers who want to become faster and more efficient in their work.
+          I publish technical content for developers who want to skill up
         </p>
         <div className="mt-6 flex">
           <input
