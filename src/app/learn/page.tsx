2328f1be55624b0971fc6aab8139757588776fb4
Zachary Proser <zackproser@gmail.com>
2023-11-04 11:54:46 -0400

    Fix authOptions passing

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth/auth-options";
 
 import { SimpleLayout } from '@/components/SimpleLayout';
 import GithubSignin from '@/components/github-signin';
