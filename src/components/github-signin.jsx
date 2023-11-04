91914e2dc377f52c0a0ec623dad746bfdcce2905
Zachary Proser <zackproser@gmail.com>
2023-11-04 11:45:06 -0400

    Render courses on index page when logged in

---
+++ src/components/github-signin.jsx
@@ -0,0 +1,19 @@
+'use client';
+
+import Image from "next/image";
+import Link from "next/link";
+import { signIn } from "next-auth/react";
+import githubSignin from '@/images/github-signin.png';
+
+export default function Component() {
+          const handleClick = (e) => {
+                    e.preventDefault();
+                    signIn();
+          };
+
+          return (
+                    <Link onClick={handleClick} href="/learn" passHref>
+                              <Image width={1200} src={githubSignin} alt="Github Signin" />
+                    </Link>
+          );
+}
