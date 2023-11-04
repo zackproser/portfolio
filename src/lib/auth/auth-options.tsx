3f6a19c2f5c35269265e25c2532b2b0f6b5e83db
Zachary Proser <zackproser@gmail.com>
2023-11-04 11:54:46 -0400

    Fix authOptions passing

---
+++ src/lib/auth/auth-options.tsx
@@ -0,0 +1,22 @@
+
+import { type AuthOptions } from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+// export this because react server components also need it in order to call 
+// getServerSession
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log(`signIn callback: %o, %o, %o, %o, %o`, user, account, profile, email, credentials)
+      return true
+    }
+  }
+} as AuthOptions
+
+
