a261bbd4b58bac1b1f519695aa6581294214b1ba
Zachary Proser <zackproser@gmail.com>
2023-11-04 14:16:50 -0400

    Checkpoint: courses metadata rendered in sidebar

--- src/lib/courses.ts
+++ src/lib/courses.ts
@@ -1,5 +1,7 @@
 import { Article, ArticleWithSlug } from './shared-types'
 import glob from 'fast-glob'
+import fs from 'fs'
+import path from 'path'
 
 async function importArticle(
   articleFilename: string,
@@ -25,4 +27,13 @@ export async function getAllCourses() {
   let articles = await Promise.all(articleFilenames.map(importArticle))
 
   return articles.sort((a, z) => +new Date(z.date) - +new Date(a.date))
+}
+
+export async function getCourseSegments(course: string) {
+  const segmentDirs = fs.readdirSync(path.join(process.cwd(), `src/app/learn/courses/${course}`)).filter((dir: string) => dir != 'page.mdx');
+  const segmentMetas = await Promise.all(segmentDirs.map(async (dir) => {
+    const { meta } = await import(`src/app/learn/courses/${course}/${dir}/page.mdx`);
+    return meta;
+  }));
+  return segmentMetas.filter(meta => meta != undefined);
 }
