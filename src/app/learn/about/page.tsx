c919051b9d4120e924c07f1930c157b9174a1528
Zachary Proser <zackproser@gmail.com>
2023-11-11 09:40:52 -0500

    Drop grey background form learn/about page features section

--- src/app/learn/about/page.tsx
+++ src/app/learn/about/page.tsx
@@ -33,7 +33,7 @@ const features: Feature[] = [
 
 function Features({ features }: FeaturesProps) {
   return (
-    <div className="bg-white dark:bg-gray-800">
+    <div className="bg-white dark:bg-black">
       <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
         <div>
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
