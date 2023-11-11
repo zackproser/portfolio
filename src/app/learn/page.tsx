305253122bf040993d729c021ef19a61518990e9
Zachary Proser <zackproser@gmail.com>
2023-11-11 11:44:48 -0500

    Separate CourseIndex into a component

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -4,24 +4,15 @@ import { authOptions } from "@/lib/auth/auth-options";
 import { SimpleLayout } from '@/components/SimpleLayout';
 import GithubSignin from '@/components/github-signin';
 
-import { type ArticleWithSlug } from '@/lib/shared-types';
-
 import Image from 'next/image';
-
-
 import { Container } from '@/components/Container'
-
-
-import { getAllCourses } from '@/lib/courses'
-import { BlogPostCard } from '@/components/BlogPostCard'
-
+import { Button } from '@/components/Button';
+import CourseIndex from '@/components/CourseIndex';
 
 import zpSchoolForHackers from '@/images/zp-school-for-hackers.png'
 
-
 export default async function LearningHome() {
   const session = await getServerSession(authOptions);
-  const courses = session ? await getAllCourses() : [];
 
   if (!session) {
     return (
@@ -44,14 +35,20 @@ export default async function LearningHome() {
           <Image src={zpSchoolForHackers} alt="Zachary Proser's School for Hackers" />
         </div>
         <p className="mt-6 text-lg leading-8 text-gray-300">
-          <h2 className="text-2l font-bold tracking-tight text-white sm:text-4xl">Something big is coming...</h2>
+          <h2 className="text-2l font-bold tracking-tight text-white sm:text-4xl">Learn more</h2>
+          What is the Zachary Proser School for Hackers?<br />
+          <Button
+            className="mt-4"
+            variant="green"
+            href={`/learn/about`}
+          >
+            Learn more
+          </Button>
         </p>
-
-        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {courses.map((article: ArticleWithSlug) => (
-            <BlogPostCard key={article.slug} article={article} />
-          ))}
-        </div>
+        <p className="mt-6 text-lg leading-8 text-gray-300">
+          <h2 className="text-2l font-bold tracking-tight text-white sm:text-4xl">Courses</h2>
+        </p>
+        <CourseIndex />
       </div>
     </SimpleLayout>
   )
