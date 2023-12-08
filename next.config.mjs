3ddc23465d0b60a8785425cb28ea682fb49eb5df
Zachary Proser <zackproser@gmail.com>
2023-12-08 18:46:08 +0000

    New post: SF meetup 12-6-2023 at a16z

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import rehypePrism from '@mapbox/rehype-prism'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['react-tweet'],
   experimental: {
     appDir: true,
   },
