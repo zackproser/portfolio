307003f2b864b59f3674c4f3c247325168b39199
Zachary Proser <zackproser@gmail.com>
2024-01-14 15:59:48 -0500

    Get Stripe checkout embedded form rendering

---
+++ src/app/test/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout
+} from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string)
+
+const SomePage = () => {
+  const [clientSecret, setClientSecret] = useState('');
+
+  useEffect(() => {
+    fetch("/api/checkout-sessions", {
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  return (
+    <div id="checkout">
+      {clientSecret && (
+        <EmbeddedCheckoutProvider
+          stripe={stripePromise}
+          options={{ clientSecret }}
+        >
+          <EmbeddedCheckout />
+        </EmbeddedCheckoutProvider>
+      )}
+    </div>
+  )
+};
+
+export default SomePage;
+
