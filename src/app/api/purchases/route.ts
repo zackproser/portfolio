360c844830be7a51493707c1606a7b4c6c4ba0d1
Zachary Proser <zackproser@gmail.com>
2024-01-15 10:10:34 -0500

    "Add course purchase function and update success page"

---
+++ src/app/api/purchases/route.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { sql } from '@vercel/postgres';
+
+export async function POST(req: NextRequest) {
+
+  console.log(`[POST] /api/payments`)
+
+  const { sessionId, customerEmail, courseId } = await req.json()
+
+  console.log(`[POST] sessionId: ${sessionId}`)
+  console.log(`[POST] customerEmail: ${customerEmail}`)
+  console.log(`[POST] courseId: ${courseId}`)
+
+  const stripePaymentResult = await sql`
+  INSERT INTO StripePayments (
+    student_id, 
+    stripe_payment_id,
+    amount,
+    payment_status
+  )
+  VALUES (
+    (SELECT student_id FROM Students WHERE email = ${customerEmail}),
+    ${sessionId},
+    -- Get amount from Stripe API  
+    10,
+    'paid'
+  )
+  RETURNING *  
+`
+
+  const courseEnrollmentResult = await sql`
+  INSERT INTO CourseEnrollments (student_id, course_id)
+  VALUES (
+    (SELECT student_id FROM Students WHERE email = ${customerEmail}),
+    ${courseId}
+  )
+  RETURNING *
+`
+
+  return NextResponse.json({
+    message: 'Database updated successfully'
+  }, {
+    status: 200,
+  })
+
+}
