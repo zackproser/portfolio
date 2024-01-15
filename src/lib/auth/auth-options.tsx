a21e323f20f38305625f641760e14e37ac99e86f
Zachary Proser <zackproser@gmail.com>
2024-01-15 11:28:59 -0500

    "Added ability to check for existing student records, create new student if none exists, and fetch student details from Github during authentication"

--- src/lib/auth/auth-options.tsx
+++ src/lib/auth/auth-options.tsx
@@ -21,6 +21,8 @@ export const authOptions = {
 
       // Extract GitHub profile ID from 'account' 
       const githubUsername: string = profile && profile.login! ? profile.login! : 'unknown';
+      const userFullName: string = profile && profile.name ? profile.name : 'unknown';
+      const userEmailAddress: string = profile && profile.email ? profile.email : 'unknown';
 
       // Insert login record into 'logins' table
       try {
@@ -31,7 +33,36 @@ export const authOptions = {
         console.error('Error inserting login record:', error);
       }
 
+      // Check if student record already exists
+      const existingStudent = await sql`
+      SELECT * 
+      FROM students 
+      WHERE github_username = ${githubUsername}
+    `;
+
+      let studentId;
+
+      if (existingStudent.rowCount > 0) {
+        // Student found, use id
+        studentId = existingStudent.rows[0].student_id;
+      } else {
+        // Create new student 
+        const createRes = await sql`
+        INSERT INTO students (github_username, full_name, email)
+        VALUES (${githubUsername}, ${userFullName}, ${userEmailAddress})
+        RETURNING student_id
+      `;
+        studentId = createRes.rows[0].student_id;
+      }
+
       return true;
+    },
+    async session({ session, user, token }) {
+      console.log(`session method callback: %o, %o, %o`, session, user, token)
+      return {
+        wakka: true,
+        ...session,
+      }
     }
   }
 } as AuthOptions;
