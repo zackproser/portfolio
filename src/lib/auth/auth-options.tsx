24852e8d5dd760b20259b67bb17a3f23e646a5f5
Zachary Proser <zackproser@gmail.com>
2024-01-16 22:48:38 -0500

    Implement user purchase validation and course access control
    This commit adds the logic in page.tsx to validate if a user has made a purchase for the course they are trying to access. If not, the user is redirected to the checkout page for that course. If the course does not exist, a 'not-found' page is returned. This commit also moves the getUserIdFromEmail and getPurchasedCourses functions from auth-options.tsx to queries.ts. Furthermore, LearnPage() in page.tsx no longer depends on the session to map over the purchased courses. The course_id is also being selected now in getProductDetails.

--- src/lib/auth/auth-options.tsx
+++ src/lib/auth/auth-options.tsx
@@ -1,6 +1,7 @@
 import { sql } from "@vercel/postgres";
 import { type AuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { getUserIdFromEmail, getPurchasedCourses } from "@/lib/queries";
 
 declare module "next-auth" {
   interface Profile {
@@ -78,16 +79,4 @@ export const authOptions = {
   }
 } as AuthOptions;
 
-async function getUserIdFromEmail(email: string): Promise<number | null> {
-  // Implement logic to fetch user ID from the database based on email
-  // Return the user ID or null if not found
-  const userRes = await sql`SELECT student_id FROM students WHERE email = ${email}`;
-  return userRes.rowCount > 0 ? userRes.rows[0].student_id : null;
-}
 
-async function getPurchasedCourses(userId: number): Promise<number[]> {
-  // Implement logic to fetch purchased courses from the database
-  // Return an array of course IDs or details
-  const purchasesRes = await sql`SELECT course_id from CourseEnrollments WHERE student_id = ${userId}`;
-  return purchasesRes.rows.map(row => row.course_id);
-}
