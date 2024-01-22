c151735ffc12e456a3a711f6ebb525634b4388b7
Zachary Proser <zackproser@gmail.com>
2024-01-21 21:42:53 -0500

    Checkout experience. Add products API and waitinglist capture

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,78 +1,109 @@
-'use client';
+"use client";
+import React, { useEffect, useState } from "react";
+import { Container } from "@/components/Container";
+import { useSession } from "next-auth/react";
 
-import React, { useEffect, useState } from 'react';
-import { Container } from '@/components/Container'
-import { useSession } from "next-auth/react"
+import { CourseStatus } from "@/utils/productUtils";
 
-import { loadStripe } from '@stripe/stripe-js';
-import { useSearchParams } from 'next/navigation'
+import { loadStripe } from "@stripe/stripe-js";
+import { useSearchParams } from "next/navigation";
 
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
 
 import {
-  EmbeddedCheckoutProvider,
-  EmbeddedCheckout
-} from '@stripe/react-stripe-js';
+	EmbeddedCheckoutProvider,
+	EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string)
+const stripePromise = loadStripe(
+	process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string,
+);
 
 const CheckoutPage = () => {
+	const searchParams = useSearchParams();
+	const { data: session, status } = useSession();
+	console.log(`CheckoutPage: session: ${JSON.stringify(session)}`);
 
-  const { data: session, status } = useSession();
+	const productSlug = searchParams.get("product");
 
-  console.log(`CheckoutPage: session: ${JSON.stringify(session)}`);
+	const [clientSecret, setClientSecret] = useState("");
+	const [productTitle, setProductTitle] = useState("");
+	const [productStatus, setProductStatus] = useState("");
+	const [userEmail, setUserEmail] = useState("");
 
-  const searchParams = useSearchParams();
-  const [clientSecret, setClientSecret] = useState('');
+	// If user is not signed in, redirect them to sign in page
+	useEffect(() => {
+		if (status === "unauthenticated" || session === null) {
+			return redirect("/api/auth/signin");
+		}
+	}, [status, session]);
 
-  // Redirect if not logged in
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      redirect('/api/auth/signin');
-    }
-  }, [status]);
+	// If user is signed in, get their email
+	useEffect(() => {
+		if (session) {
+			setUserEmail(session.user.email as unknown as string);
+		}
+	}, [session]);
 
+	// Product validity check: ensure product is available for sale
+	// Otherwise, redirect user to the product-specific waitinglist
+	useEffect(() => {
+		fetch(`/api/products?product=${productSlug}`).then((res) => {
+			res.json().then((data) => {
+				console.log(`data: ${JSON.stringify(data)}`);
 
-  // Fetch client secret if logged in and product specified
-  useEffect(() => {
-    const product = searchParams.get('product');
+				const { title, status } = data;
 
-    const payload = {
-      product
-    }
+				setProductTitle(title);
+				setProductStatus(status);
+			});
+		});
 
-    if (product) {
-      fetch(`/api/checkout-sessions`, {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
-      })
-        .then((res) => res.json())
-        .then((data) => setClientSecret(data.clientSecret));
-    }
+		console.log(`productStatus: ${productStatus}`);
 
-  }, [session, searchParams]);
+		if (
+			productStatus === CourseStatus.InProgress ||
+			productStatus === CourseStatus.ComingSoon
+		) {
+			return redirect(
+				`/waitinglist?product=${productSlug}&productName=${productTitle}&email=${userEmail}`,
+			);
+		}
+	}, [productSlug, productTitle, productStatus, userEmail]);
 
-  return (
-    <Container className="mt-16 sm:mt-32">
+	// Fetch client secret if logged in and product specified
+	useEffect(() => {
+		const payload = {
+			product: productSlug,
+		};
 
-      <div id="checkout" className="bg-zinc-50 dark:bg-black">
+		if (productSlug) {
+			fetch("/api/checkout-sessions", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(payload),
+			})
+				.then((res) => res.json())
+				.then((data) => setClientSecret(data.clientSecret));
+		}
+	}, [productSlug]);
 
-        {clientSecret && (
-          <EmbeddedCheckoutProvider
-            stripe={stripePromise}
-            options={{ clientSecret }}
-          >
-            <EmbeddedCheckout />
-          </EmbeddedCheckoutProvider>
-        )}
-
-      </div>
-
-    </Container>
-  )
+	return (
+		<Container className="mt-16 sm:mt-32">
+			<div id="checkout" className="bg-zinc-50 dark:bg-black">
+				{clientSecret && (
+					<EmbeddedCheckoutProvider
+						stripe={stripePromise}
+						options={{ clientSecret }}
+					>
+						<EmbeddedCheckout />
+					</EmbeddedCheckoutProvider>
+				)}
+			</div>
+		</Container>
+	);
 };
 
 export default CheckoutPage;
