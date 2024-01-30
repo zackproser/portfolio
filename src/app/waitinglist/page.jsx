58fef19e414c22c18efd8623fd75c0c97d9dd69c
Zachary Proser <zackproser@gmail.com>
2024-01-30 13:22:31 -0500

    Extend waitinglist page to support no product. Re-point /learn

--- src/app/waitinglist/page.jsx
+++ src/app/waitinglist/page.jsx
@@ -17,28 +17,35 @@ export default function Page() {
   useEffect(() => {
     const email = searchParams.get("email") || "";
     const slug = searchParams.get("product") || "";
-    const productName = searchParams.get("productName") || "";
+    const productName = searchParams.get("productName") || null;
     setUserEmail(email);
     setProductSlug(slug);
-    setProductName(productName);
+    setProductName(productName ?? "Zachary Proser's School for Hackers");
   }, [searchParams]);
 
   return (
     <Container className="mt-16 sm:mt-32">
       <h1 className="text-3xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-        Hand-crafted project-based learning takes time...
+        Your thirst for knowledge is commendable!
       </h1>
+
+
       <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-        So, we truly appreciate your patience. If you like, you can subscribe to
-        the waitinglist here, by clicking the Count me in button, and we&apos;ll
-        reach out as soon as the{" "}
-        <span className="font-semibold">{productName}</span> course is ready.
+        But hand-crafted project-based learning takes time.
       </p>
+
+
+      <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+        Enter your email below to secure your spot on the waiting list, and we&apos;ll let you know as soon as{" "}
+        <span className="font-semibold">{productName}</span> is ready.
+      </p>
+
       <ProductWaitinglistForm
         userEmail={userEmail}
         productSlug={productSlug}
         productName={productName}
       />
+
     </Container>
   );
 }
