5394d50931075c565c90097f59ff01b3ab6ac6fb
Zachary Proser <zackproser@gmail.com>
2024-01-30 09:43:23 -0500

    Define receipt email types and improve purchase API route

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -4,36 +4,36 @@ import { authOptions } from "@/lib/auth/auth-options";
 import { getProductDetails, ProductDetails } from "@/utils/productUtils";
 
 export async function GET(req: NextRequest) {
-  console.log("GET /products");
+	console.log("GET /products");
 
-  const session = await getServerSession(authOptions);
+	const session = await getServerSession(authOptions);
 
-  if (!session) {
-    return new NextResponse(
-      JSON.stringify({ error: "Not signed into GitHub" }),
-      {
-        status: 403,
-      },
-    );
-  }
+	if (!session) {
+		return new NextResponse(
+			JSON.stringify({ error: "Not signed into GitHub" }),
+			{
+				status: 403,
+			},
+		);
+	}
 
-  const productSlug = req.nextUrl.searchParams.get("product");
+	const productSlug = req.nextUrl.searchParams.get("product");
 
-  if (!productSlug) {
-    return new NextResponse(
-      JSON.stringify({ error: "Must supply valid product slug" }),
-      {
-        status: 400,
-      },
-    );
-  }
+	if (!productSlug) {
+		return new NextResponse(
+			JSON.stringify({ error: "Must supply valid product slug" }),
+			{
+				status: 400,
+			},
+		);
+	}
 
-  const productDetails: ProductDetails | null =
-    await getProductDetails(productSlug);
+	const productDetails: ProductDetails | null =
+		await getProductDetails(productSlug);
 
-  if (!productDetails) {
-    throw new Error(`Could not find product with slug: ${productSlug}`);
-  }
+	if (!productDetails) {
+		throw new Error(`Could not find product with slug: ${productSlug}`);
+	}
 
-  return NextResponse.json({ ...productDetails }, { status: 200 });
+	return NextResponse.json({ ...productDetails }, { status: 200 });
 }
