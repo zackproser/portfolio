d12acc1aa7c6786ca3c621354f700ed7e5c2176d
Zachary Proser <zackproser@gmail.com>
2024-02-12 09:49:46 -0500

    Dynamic metadata generation for the win. Begin converting pages

---
+++ src/utils/createMetadata.js
@@ -0,0 +1,20 @@
+import { generateOgUrl } from '@/utils/ogUrl'
+
+export function createMetadata({ author, date, title, description, image }) {
+  const baseMeta = { author, date, title, description, image };
+  return {
+    ...baseMeta,
+    openGraph: {
+      title,
+      description,
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}`,
+      siteName: "Zack Proser portfolio",
+      images: [
+        {
+          url: generateOgUrl({ title, description }),
+        },
+      ],
+    },
+  };
+}
+
