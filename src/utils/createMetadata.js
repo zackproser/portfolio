43b7be96d1154ab2a714aa1029a8ebad7536123b
Zachary Proser <zackproser@gmail.com>
2024-02-12 12:51:11 -0500

    Fill out other high-priority metadata fields

--- src/utils/createMetadata.js
+++ src/utils/createMetadata.js
@@ -4,17 +4,45 @@ export function createMetadata({ author, date, title, description, image }) {
   const baseMeta = { author, date, title, description, image };
   return {
     ...baseMeta,
+    metadataBase: new URL('https://zackproser.com'),
+    category: 'technology',
+    generator: 'Next.js',
+    referrer: 'origin-when-cross-origin',
+    keywords: ['AI engineer', 'Generative AI', 'AI consultant', 'Staff AI developer'],
+    authors: [{ name: 'Zachary Proser' }],
+    creator: 'Zachary Proser',
+    publisher: 'Zachary Proser',
     openGraph: {
       title,
       description,
       url: `${process.env.NEXT_PUBLIC_SITE_URL}`,
-      siteName: "Zack Proser portfolio",
+      siteName: 'Zack Proser portfolio',
       images: [
         {
           url: generateOgUrl({ title, description, image }),
         },
       ],
+      locale: 'en_US',
+      type: 'website',
     },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        'max-video-preview': -1,
+        'max-image-preview': 'large',
+        'max-snippet': -1,
+      },
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      creator: '@zackproser',
+      images: [generateOgUrl({ title, description, image })],
+    }
   };
 }
 
