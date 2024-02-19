1dad9d334ec5055bfb34bba5ac59759ee6070634
Zachary Proser <zackproser@gmail.com>
2024-02-19 17:12:06 -0500

    Hack to set darkmode theme on site by default

---
+++ src/app/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                // Immediately invoked function to set the theme before React loads
+                (function() {
+                  // Default to dark mode
+                  document.body.classList.add('dark');
+                })();
+              `,
+            }}
+          />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
