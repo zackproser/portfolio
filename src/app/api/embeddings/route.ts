d4ea536c285f097e82e6b10f6240ab555136fdda
Zachary Proser <zackproser@gmail.com>
2024-03-10 13:10:18 -0400

    Basics rendered to screen

---
+++ src/app/api/embeddings/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server';
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(req: NextRequest) {
+  const { inputText } = await req.json();
+
+  try {
+    const response = await openai.embeddings.create({
+      model: 'text-embedding-3-small',
+      input: inputText,
+    });
+
+    const generatedEmbeddings = response.data[0].embedding;
+    console.log(`Generated embeddings: %o`, generatedEmbeddings);
+    return NextResponse.json({ embeddings: generatedEmbeddings }, { status: 200 })
+  } catch (error) {
+    console.error('Error generating embeddings:', error);
+    return NextResponse.json({ error }, { status: 500 })
+  }
+}
