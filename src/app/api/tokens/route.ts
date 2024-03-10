4635aee2bb4090e1d896bb264883b8f201adcece
Zachary Proser <zackproser@gmail.com>
2024-03-10 14:18:47 -0400

    Checkpoint

---
+++ src/app/api/tokens/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { Tiktoken } from "tiktoken/lite";
+import cl100k_base from "tiktoken/encoders/cl100k_base.json";
+
+const encoding = new Tiktoken(
+  cl100k_base.bpe_ranks,
+  cl100k_base.special_tokens,
+  cl100k_base.pat_str
+);
+
+export async function POST(req: NextRequest) {
+  const { inputText } = await req.json();
+
+  try {
+    if (!encoding) {
+      throw new Error('Encoding not loaded');
+    }
+
+    const tokens = encoding.encode(inputText);
+    console.log(`Generated tokens: %o`, tokens);
+
+    return NextResponse.json({ tokens: Array.from(tokens) }, { status: 200 });
+  } catch (error) {
+    console.error('Error generating tokens:', error);
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
