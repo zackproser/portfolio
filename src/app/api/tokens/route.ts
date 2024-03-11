c94105d5f302174fe8e940cdb9bc8dff9a764ac4
Zachary Proser <zackproser@gmail.com>
2024-03-11 15:18:04 -0400

    Swap in js-tiktoken

--- src/app/api/tokens/route.ts
+++ src/app/api/tokens/route.ts
@@ -1,29 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { Tiktoken } from "tiktoken/lite";
-import cl100k_base from "tiktoken/encoders/cl100k_base.json";
-
-const encoding = new Tiktoken(
-  cl100k_base.bpe_ranks,
-  cl100k_base.special_tokens,
-  cl100k_base.pat_str
-);
+import { encodingForModel } from "js-tiktoken";
 
 export async function POST(req: NextRequest) {
-  const { inputText } = await req.json();
 
   try {
-    if (!encoding) {
-      throw new Error('Encoding not loaded');
-    }
+    const enc = encodingForModel('gpt-3.5-turbo');
 
-    console.log(`TEST`)
-    console.log(`inputText: ${inputText}`)
-    const tokens = encoding.encode(inputText);
-    console.log(`tokens: %o`, tokens);
+    const { inputText } = await req.json();
+    console.log(`inputText: ${inputText}`);
+
+    const tokens = enc.encode(inputText);
 
     return NextResponse.json({ tokens }, { status: 200 });
   } catch (error) {
-    console.error('Error generating tokens:', error);
     return NextResponse.json({ error }, { status: 500 });
   }
 }
