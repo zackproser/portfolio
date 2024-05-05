d1416a7ed63f872ff6f9e60225085258b34a6257
Zachary Proser <zackproser@gmail.com>
2024-05-05 13:47:04 -0400

    Fix type errors

--- src/components/SparkleNavItem.js
+++ src/components/SparkleNavItem.js
@@ -4,11 +4,12 @@ import Link from 'next/link';
 const SparkleNavItem = function({
   href,
   children,
+  className = '',
   glowColor = '#FFD700' // Default gold color
 }) {
   return (
     <li className="relative group" style={{ '--glow-color': glowColor }}>
-      <Link href={href} className="block px-3 py-2 text-lg font-bold transition duration-300 ease-in-out transform group-hover:scale-105 glow">
+      <Link href={href} className={className ?? "block px-3 py-2 text-lg font-bold transition duration-300 ease-in-out transform group-hover:scale-105 glow"}>
         {children}
       </Link>
       <div className={`absolute top-0 right-0 sparkle ${glowColor === '#3fe291' ? 'sparkle-green' : 'sparkle-default'}`}></div>
