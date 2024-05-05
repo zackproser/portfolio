f4389b1f078965af2a48e988f680b857e95ab800
Zachary Proser <zackproser@gmail.com>
2024-05-05 14:04:20 -0400

    Set both hire and learn to default glow for now

--- src/components/SparkleNavItem.js
+++ src/components/SparkleNavItem.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
 
+const variants = {
+  none: '',
+  green: 'whitespace-nowrap px-4 py-2 text-sm md:text-base bg-green-500 text-white rounded-full shadow-md'
+}
+
+const greenGlow = '#3fe291'
+
 const SparkleNavItem = function({
   href,
   children,
-  className = '',
+  variant = 'none',
   glowColor = '#FFD700' // Default gold color
 }) {
   return (
-    <li className="relative group" style={{ '--glow-color': glowColor }}>
-      <Link href={href} className={className ?? "block px-3 py-2 text-lg font-bold transition duration-300 ease-in-out transform group-hover:scale-105 glow"}>
+    <li className="relative group" style={{ '--glow-color': variant === 'green' ?  greenGlow : glowColor }}>
+      <Link href={href} className={variant === 'green' ? variants.green : "block px-3 py-2 text-lg font-bold transition duration-300 ease-in-out transform group-hover:scale-105 glow"}>
         {children}
       </Link>
-      <div className={`absolute top-0 right-0 sparkle ${glowColor === '#3fe291' ? 'sparkle-green' : 'sparkle-default'}`}></div>
+      <div className={`absolute top-0 right-0 ${variant === 'green' ? 'sparkle-green' : 'sparkle'}`}></div>
     </li>
   );
 };
