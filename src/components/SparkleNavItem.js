866358647bd42e24f5f5d7db5ab582e7540ab1b6
Zachary Proser <zackproser@gmail.com>
2024-05-05 13:23:06 -0400

    Add green glowing nav variant. Add hire me link - remove others

--- src/components/SparkleNavItem.js
+++ src/components/SparkleNavItem.js
@@ -3,17 +3,15 @@ import Link from 'next/link';
 
 const SparkleNavItem = function({
   href,
-  title,
-  children
+  children,
+  glowColor = '#FFD700' // Default gold color
 }) {
   return (
-    <li className="relative group">
-      <Link href={href}>
-        <span className="block px-3 py-2 text-lg font-bold transition duration-300 ease-in-out transform group-hover:scale-105 glow">
-          {children}
-        </span>
+    <li className="relative group" style={{ '--glow-color': glowColor }}>
+      <Link href={href} className="block px-3 py-2 text-lg font-bold transition duration-300 ease-in-out transform group-hover:scale-105 glow">
+        {children}
       </Link>
-      <div className="absolute top-0 right-0 sparkle"></div>
+      <div className={`absolute top-0 right-0 sparkle ${glowColor === '#3fe291' ? 'sparkle-green' : 'sparkle-default'}`}></div>
     </li>
   );
 };
