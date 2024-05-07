e9eae84aec8b29ad46a16d690b82e7893c479377
Zachary Proser <zackproser@gmail.com>
2024-05-07 13:03:26 -0400

    Fix capitalization

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -185,7 +185,7 @@ function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
       <ul className="flex flex-nowrap rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <SparkleNavItem href="https://sponsor.zackproser.com">Hire</SparkleNavItem>
         <NavItem href="/blog">Blog</NavItem>
-        <NavItem href="/collections">collections</NavItem>
+        <NavItem href="/collections">Collections</NavItem>
         <NavItem href="/videos">Videos</NavItem>
         <NavItem href="/newsletter">Newsletter</NavItem>
         <NavItem href="/projects">Projects</NavItem>
