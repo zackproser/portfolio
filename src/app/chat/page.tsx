f494767e2bc1cf212c29ce9a56b5dcd481f16be6
Zachary Proser <zackproser@gmail.com>
2024-05-10 09:06:01 -0400

    HOORAH - blog posts have entered the chat

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,11 +1,29 @@
 'use client';
 
 import { useChat } from 'ai/react';
-import { clsx } from 'clsx'
-import { SimpleLayout } from '@/components/SimpleLayout'
+import { useState } from 'react';
+import { clsx } from 'clsx';
+import { SimpleLayout } from '@/components/SimpleLayout';
+import Image from 'next/image';
+import { BlogPostCard } from '@/components/BlogPostCard';
+import { ArticleWithSlug } from '@/lib/shared-types';
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const [article, setArticle] = useState<ArticleWithSlug | null>(null);
+  const [heroImage, setHeroImage] = useState('');
+
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    onResponse(response) {
+      const sourcesHeader = response.headers.get('x-sources');
+      const parsedArticle: ArticleWithSlug | null = sourcesHeader
+        ? (JSON.parse(atob(sourcesHeader as string)) as ArticleWithSlug)
+        : null;
+      console.log(`parsedArticle: %o`, parsedArticle);
+      setArticle(parsedArticle);
+    },
+    headers: {},
+  });
+
   return (
     <SimpleLayout
       title="Chat with my writing!"
@@ -13,19 +31,29 @@ export default function Chat() {
     >
       <div className="flex flex-col flex-1 w-full max-w-3xl mx-auto">
         <div className="flex-1 px-6 py-4 overflow-y-scroll">
-          {messages.map(m => (
-            <div key={m.id} className="mb-4 whitespace-pre-wrap text-lg leading-relaxed">
-              <span className={clsx("font-bold", {
-                "text-blue-700": m.role === 'user',
-                "text-green-700": m.role !== 'user'
-              })}>
-                {m.role === 'user' ? 'You: ' : 'The Ghost of Zachary Proser\'s Writing: '}
+          {messages.map((m) => (
+            <div
+              key={m.id}
+              className="mb-4 whitespace-pre-wrap text-lg leading-relaxed"
+            >
+              <span
+                className={clsx('font-bold', {
+                  'text-blue-700': m.role === 'user',
+                  'text-green-700': m.role !== 'user',
+                })}
+              >
+                {m.role === 'user'
+                  ? 'You: '
+                  : "The Ghost of Zachary Proser's Writing: "}
               </span>
               {m.content}
             </div>
           ))}
+          {heroImage && (
+            <Image src={heroImage} width={300} height={300} alt="Hero Image" />
+          )}
+          {article && <BlogPostCard key={article.slug} article={article} />}
         </div>
-
         <form onSubmit={handleSubmit} className="mt-auto mb-8">
           <input
             className="w-full p-2 border border-gray-300 rounded shadow-xl"
