bd5033ebcea3c53f0786961d886c95a45a128489
Zachary Proser <zackproser@gmail.com>
2024-05-10 17:43:12 -0400

    Improvements

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -6,24 +6,29 @@ import { clsx } from 'clsx';
 import { SimpleLayout } from '@/components/SimpleLayout';
 import { BlogPostCard } from '@/components/BlogPostCard';
 import { ArticleWithSlug } from '@/lib/shared-types';
+import { LoadingAnimation } from '@/components/LoadingAnimation';
 
 const prepopulatedQuestions = [
   "What is the programming bug?",
   "Why do you love Next.js so much?",
   "What do you do at Pinecone?",
+  "How can I become a better developer?",
+  "What is ggshield and why is it important?"
 ];
 
 export default function Chat() {
-  const [article, setArticle] = useState<ArticleWithSlug | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [articles, setArticles] = useState<ArticleWithSlug[]>([]);
 
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
+  const { messages, input, setInput, handleInputChange, handleSubmit } = useChat({
     onResponse(response) {
       const sourcesHeader = response.headers.get('x-sources');
-      const parsedArticle: ArticleWithSlug | null = sourcesHeader
-        ? (JSON.parse(atob(sourcesHeader as string)) as ArticleWithSlug)
-        : null;
-      console.log(`parsedArticle %o`, parsedArticle);
-      setArticle(parsedArticle);
+      const parsedArticles: ArticleWithSlug[] = sourcesHeader
+        ? (JSON.parse(atob(sourcesHeader as string)) as ArticleWithSlug[])
+        : [];
+      console.log(`parsedArticle %o`, parsedArticles);
+      setArticles(parsedArticles);
+      setIsLoading(false);
     },
     headers: {},
     onFinish() {
@@ -34,6 +39,11 @@ export default function Chat() {
       });
     }
   });
+
+  const userFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    setIsLoading(true); // Set loading state here
+    handleSubmit(e);
+  };
 
   const handlePrepopulatedQuestion = (question: string) => {
     handleInputChange({
@@ -47,12 +57,14 @@ export default function Chat() {
       event_label: question,
     });
 
+    setIsLoading(true); // Set loading state here to indicate submission is processing
+
     const customSubmitEvent = {
       preventDefault: () => { },
     } as unknown as React.FormEvent<HTMLFormElement>;
-    setTimeout(() => {
-      handleSubmit(customSubmitEvent);
-    }, 0);
+
+    // Submit immediately after updating the input
+    handleSubmit(customSubmitEvent);
   };
 
   return (
@@ -60,6 +72,7 @@ export default function Chat() {
       title="Chat with my writing!"
       intro="This experience uses Pinecone, OpenAI and LangChain..."
     >
+      {isLoading && (<LoadingAnimation />)}
       <div className="flex flex-col md:flex-row flex-1 w-full max-w-5xl mx-auto">
         <div className="flex-1 px-6">
           {messages.map((m) => (
@@ -82,10 +95,12 @@ export default function Chat() {
           ))}
         </div>
         <div className="md:w-1/3 px-6 py-4">
-          {article && (
-            <div>
+          {Array.isArray(articles) && (articles.length > 0) && (
+            <div className="">
               <h3 className="mb-4 text-xl font-semibold">Related Posts</h3>
-              <BlogPostCard key={article.slug} article={article} />
+              {(articles as ArticleWithSlug[]).map((article) => (
+                <BlogPostCard key={article.slug} article={article} />
+              ))}
             </div>
           )}
         </div>
@@ -93,11 +108,11 @@ export default function Chat() {
       <div className="mt-4 px-6">
         <h3 className="mb-2 text-lg font-semibold">Example Questions:</h3>
         <p>Double-click to ask one of these questions, or type your own below and hit enter.</p>
-        <div className="space-x-2 mb-4">
+        <div className="flex flex-wrap justify-center gap-2 mb-4">
           {prepopulatedQuestions.map((question, index) => (
             <button
               key={index}
-              className="px-3 py-1 bg-blue-500 text-white rounded"
+              className="px-3 py-2 bg-blue-500 text-white rounded shadow hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50"
               onClick={() => handlePrepopulatedQuestion(question)}
             >
               {question}
@@ -105,7 +120,7 @@ export default function Chat() {
           ))}
         </div>
       </div>
-      <form onSubmit={handleSubmit} className="mt-4 mb-8 px-6">
+      <form onSubmit={userFormSubmit} className="mt-4 mb-8 px-6">
         <input
           className="w-full p-2 border border-gray-300 rounded shadow-xl"
           value={input}
