700fcb8d93c7701af54730fc72d09e31d84b593d
Zachary Proser <zackproser@gmail.com>
2024-05-10 10:32:58 -0400

    Add pre-populated questions

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -7,6 +7,12 @@ import { SimpleLayout } from '@/components/SimpleLayout';
 import { BlogPostCard } from '@/components/BlogPostCard';
 import { ArticleWithSlug } from '@/lib/shared-types';
 
+const prepopulatedQuestions = [
+  "What is the programming bug?",
+  "Why do you love Next.js so much?",
+  "What do you do at Pinecone?",
+];
+
 export default function Chat() {
   const [article, setArticle] = useState<ArticleWithSlug | null>(null);
 
@@ -16,11 +22,26 @@ export default function Chat() {
       const parsedArticle: ArticleWithSlug | null = sourcesHeader
         ? (JSON.parse(atob(sourcesHeader as string)) as ArticleWithSlug)
         : null;
-      console.log(`parsedArticle: %o`, parsedArticle);
+      console.log(`parsedArticle %o`, parsedArticle);
       setArticle(parsedArticle);
     },
     headers: {},
   });
+
+  const handlePrepopulatedQuestion = (question: string) => {
+    handleInputChange({
+      target: {
+        value: question,
+      },
+    } as React.ChangeEvent<HTMLInputElement>);
+
+    const customSubmitEvent = {
+      preventDefault: () => { },
+    } as unknown as React.FormEvent<HTMLFormElement>;
+    setTimeout(() => {
+      handleSubmit(customSubmitEvent);
+    }, 0);
+  };
 
   return (
     <SimpleLayout
@@ -28,7 +49,7 @@ export default function Chat() {
       intro="This experience uses Pinecone, OpenAI and LangChain..."
     >
       <div className="flex flex-col md:flex-row flex-1 w-full max-w-5xl mx-auto">
-        <div className="flex-1 px-6 py-4">
+        <div className="flex-1 px-6">
           {messages.map((m) => (
             <div
               key={m.id}
@@ -57,7 +78,22 @@ export default function Chat() {
           )}
         </div>
       </div>
-      <form onSubmit={handleSubmit} className="mt-auto mb-8 px-6">
+      <div className="mt-4 px-6">
+        <h3 className="mb-2 text-lg font-semibold">Example Questions:</h3>
+        <p>Double-click to ask one of these questions, or type your own below and hit enter.</p>
+        <div className="space-x-2 mb-4">
+          {prepopulatedQuestions.map((question, index) => (
+            <button
+              key={index}
+              className="px-3 py-1 bg-blue-500 text-white rounded"
+              onClick={() => handlePrepopulatedQuestion(question)}
+            >
+              {question}
+            </button>
+          ))}
+        </div>
+      </div>
+      <form onSubmit={handleSubmit} className="mt-4 mb-8 px-6">
         <input
           className="w-full p-2 border border-gray-300 rounded shadow-xl"
           value={input}
