417eca8e5da4de78427ad53ae1b3fcbe6949596b
Zachary Proser <zackproser@gmail.com>
2024-06-09 14:22:57 -0400

    Minor stylistic improvements

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -10,7 +10,7 @@ export function SocialLink({ className, href, children, icon: Icon }) {
         href={href}
         className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
       >
-        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <Icon className="h-6 w-6 flex-none fill-green-500 transition group-hover:fill-teal-500" />
         <span className="ml-4">{children}</span>
       </Link>
     </li>
