49d89aa089b399b65db9d91c6bc168850c75c27c
Zachary Proser <zackproser@gmail.com>
2024-06-19 14:55:31 -0400

    Simplify homepage

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -282,7 +282,30 @@ function VideoIcon(props) {
   );
 }
 
+function MailIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path
+        fillRule="evenodd"
+        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  );
+}
+
 export {
+  MailIcon,
   MenuIcon,
   BoxIcon,
   ChevronDownIcon,
