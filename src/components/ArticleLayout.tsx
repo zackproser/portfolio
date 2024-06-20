58af4e6814c0ba063a2add81acea43a32a1575ba
Zachary Proser <zackproser@gmail.com>
2024-06-19 21:37:37 -0400

    Improve legibility

--- src/components/ArticleLayout.tsx
+++ src/components/ArticleLayout.tsx
@@ -29,7 +29,7 @@ export function ArticleLayout({
           <div className="mx-auto max-w-2xl">
             <article>
               <header className="flex flex-col">
-                <h1 className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                <h1 className="mt-3 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
                   {metadata.title}
                 </h1>
                 <time
