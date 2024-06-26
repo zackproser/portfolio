22f67ac9a597dd93166e9702d9e1ca4353f2936b
Zachary Proser <zackproser@gmail.com>
2024-06-26 07:54:22 -0400

    Fix vectordb page routing

--- src/app/vectordatabases/detail/[name]/page.jsx
+++ src/app/vectordatabases/detail/[name]/page.jsx
@@ -1,14 +1,12 @@
-'use client';
-
 import React from 'react';
+import Image from 'next/image';
 import { Container } from '@/components/Container';
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { getDatabase } from '../../../lib/getDatabases';
+import { getDatabase } from '../../../../lib/getDatabases';
+import { getEmoji } from '../../../../lib/emojiMapping';
 
 export default function DetailPage({ params }) {
   const database = getDatabase(params.name);
-
-  console.log(`database: %o`, database);
 
   if (!database) {
     return <div>Database not found</div>;
@@ -17,11 +15,16 @@ export default function DetailPage({ params }) {
   const renderSection = (title, data) => (
     <Card className="mb-4">
       <CardHeader>
-        <CardTitle>{title}</CardTitle>
+        <CardTitle>{getEmoji(title)} {title}</CardTitle>
       </CardHeader>
       <CardContent>
         {Object.entries(data).map(([key, value]) => (
-          <p key={key}>{key}: {typeof value === 'boolean' ? (value ? 'Yes' : 'No') : value.toString()}</p>
+          <p key={key}>
+            {getEmoji(key)} {key}: {' '}
+            {typeof value === 'boolean'
+              ? (value ? getEmoji('yes') : getEmoji('no'))
+              : (typeof value === 'string' ? `${value} ${getEmoji(value)}` : value.toString())}
+          </p>
         ))}
       </CardContent>
     </Card>
@@ -29,8 +32,10 @@ export default function DetailPage({ params }) {
 
   return (
     <Container>
-      <h1 className="text-2xl font-bold mb-4">{database.name} Details</h1>
-      <h1>{database.deployment.local}</h1>
+      <div className="mb-8">
+        <Image src={database.logo} alt={`${database.name} logo`} width={200} height={200} className="mx-auto" />
+        <h1 className="text-3xl font-bold text-center mt-4">{database.name}</h1>
+      </div>
       {renderSection('Deployment', database.deployment)}
       {renderSection('Scalability', database.scalability)}
       {renderSection('Data Management', database.data_management)}
