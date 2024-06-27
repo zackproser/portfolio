99aba1fd9b0da33797d9d3dc54075404d504f9a0
Zachary Proser <zackproser@gmail.com>
2024-06-27 10:03:12 -0400

    Homepage legibility improvements

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,4 +1,3 @@
-
 import Link from 'next/link'
 import clsx from 'clsx'
 
@@ -8,10 +7,10 @@ export function SocialLink({ className, href, children, icon: Icon }) {
     <li className={clsx(className, 'flex')}>
       <Link
         href={href}
-        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+        className="group flex text-sm font-medium text-green-700 dark:text-green-500 transition hover:text-green-600 dark:text-green-400 dark:hover:text-green-300"
       >
-        <Icon className="h-6 w-6 flex-none fill-green-500 transition group-hover:fill-teal-500" />
-        <span className="ml-4">{children}</span>
+        <Icon className="h-6 w-6 flex-none fill-green-500 transition group-hover:fill-green-600 dark:fill-green-400 dark:group-hover:fill-green-300" />
+        <span className="ml-4 text-base">{children}</span>
       </Link>
     </li>
   )
@@ -68,6 +67,15 @@ export function LinkedInIcon(props, fill) {
   return (
     <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
       <path fill="{ props.fill ?? 'white'}" d="M18.335 18.339H15.67v-4.177c0-.996-.02-2.278-1.39-2.278-1.389 0-1.601 1.084-1.601 2.205v4.25h-2.666V9.75h2.56v1.17h.035c.358-.674 1.228-1.387 2.528-1.387 2.7 0 3.2 1.778 3.2 4.091v4.715zM7.003 8.575a1.546 1.546 0 01-1.548-1.549 1.548 1.548 0 111.547 1.549zm1.336 9.764H5.666V9.75H8.34v8.589zM19.67 3H4.329C3.593 3 3 3.58 3 4.297v15.406C3 20.42 3.594 21 4.328 21h15.338C20.4 21 21 20.42 21 19.703V4.297C21 3.58 20.4 3 19.666 3h.003z" />
+    </svg>
+  )
+}
+
+export function MailIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" {...props}>
+      <rect x="2" y="4" width="20" height="16" rx="2" />
+      <path d="M22,5L12,13L2,5" />
     </svg>
   )
 }
