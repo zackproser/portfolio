4ef951c254b26087c3cdb18859806620a77a4220
Zachary Proser <zackproser@gmail.com>
2024-08-25 22:30:26 -0400

    Fix build error

--- src/app/collections/page.jsx
+++ src/app/collections/page.jsx
@@ -1,18 +1,13 @@
+
 import { SimpleLayout } from '@/components/SimpleLayout'
 import { BlogPostCard } from '@/components/BlogPostCard'
 import { getAllCollections } from '@/lib/collections'
-import { createMetadata } from '@/utils/createMetadata'
-
-export const metadata = createMetadata({
-  title: "Writing Collections",
-  description: "Explore curated collections of articles on topics like security, AI, and more.",
-})
 
 export default async function CollectionPage() {
   let collections = await getAllCollections()
 
   return (
-    <SimpleLayout title="Writing Collections">
+    <SimpleLayout title="Writing collections">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {collections.map(collection => (
           <BlogPostCard key={collection.slug} article={collection} />
