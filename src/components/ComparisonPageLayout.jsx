7d3ce89c1897edf2c6b55f521a90c0d33c940170
Zachary Proser <zackproser@gmail.com>
2024-08-28 11:14:13 -0400

    Move consulting CTA below fold in comparison pages

--- src/components/ComparisonPageLayout.jsx
+++ src/components/ComparisonPageLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ToolComparisonIntro from './ToolComparisonIntro';
 import { BarCharts, BusinessInfo, DetailedComparison } from './AIToolComparison';
 import NewsletterWrapper from './NewsletterWrapper';
-import { SimpleLayout }from '@/components/SimpleLayout'
+import { SimpleLayout } from '@/components/SimpleLayout';
 
 const ComparisonPageLayout = ({ tool1, tool2, proseParagraphs }) => {
   return (
@@ -10,7 +10,6 @@ const ComparisonPageLayout = ({ tool1, tool2, proseParagraphs }) => {
       title={`${tool1.name} vs ${tool2.name}`}
       intro="A detailed comparison of AI-assisted developer tools."
     >
-      <ToolComparisonIntro tool1={tool1.name} tool2={tool2.name} />
       <h2 className="text-2xl font-bold mt-8 mb-4">Growth compared</h2>
       <p>Let&apos;s start by breaking down the key metrics of each tool. You can hover over the chart for more information.</p>
       <BarCharts selectedTools={[tool1, tool2]} />
@@ -28,6 +27,7 @@ const ComparisonPageLayout = ({ tool1, tool2, proseParagraphs }) => {
         </React.Fragment>
       ))}
       <BusinessInfo selectedTools={[tool1, tool2]} />
+      <ToolComparisonIntro tool1={tool1.name} tool2={tool2.name} />
       <DetailedComparison className="mt-8 p-4 max-w-2xl mx-auto" tools={[tool1, tool2]} />
       <NewsletterWrapper 
         className="mt-8 p-4 max-w-2xl mx-auto"
