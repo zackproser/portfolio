1b8953807d710256fbcbfd1666f378c8f680f47f
Zachary Proser <zackproser@gmail.com>
2024-08-28 07:53:26 -0400

    Tweak copy on homepage

--- src/app/HomepageClientComponent.tsx
+++ src/app/HomepageClientComponent.tsx
@@ -205,10 +205,10 @@ export default function HomepageClientComponent({
             <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-8">
               <div className="flex-1 space-y-4 max-w-2xl">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl text-white">
-                  Applied Machine Learning
+                  Supercharge Your Development Workflow 
                 </h1>
                 <p className="text-lg text-gray-200 md:text-xl">
-                  Practical demos and explanations of embeddings, vector databases, neural networks. Reference architectures and projects showing MLOPS in the real world.
+                  Don't get left behind. Get the latest tools, frameworks, and techniques for AI and ML. 
                 </p>
                 <div className="w-full max-w-sm space-y-2">
                   {formSuccess ? (
