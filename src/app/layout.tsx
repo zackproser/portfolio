868ba4536ce84a9c82baa0351d958e9ad2f60bc8
Zachary Proser <zackproser@gmail.com>
2024-09-22 15:12:19 -0400

    Add post: The Rich Don't Fine-tune Like You And I

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Noto_Sans } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { SpeedInsights } from '@vercel/speed-insights/next';
@@ -7,13 +8,20 @@ import { SimpleNav } from '@/components/SimpleNav'
 import '@/styles/tailwind.css'
 import '@/styles/global.css'
 
+// Initialize the Noto Sans font
+const notoSans = Noto_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'], // Add any weights you need
+  variable: '--font-noto-sans',
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html lang="en" className={`h-full antialiased ${notoSans.variable}`}>
       <head>
         <link
           rel="alternate"
@@ -28,7 +36,7 @@ export default function RootLayout({
       </head>
       <body
         suppressHydrationWarning={true}
-        className="flex h-full bg-gray-100 dark:bg-black"
+        className={`flex h-full bg-gray-100 dark:bg-black font-sans`}
       >
         <SessionProvider>
           <Providers>
