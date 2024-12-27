bb5b81e93597d7c975f84ddaca4d4a300b068bee
= <zackproser@gmail.com>
2024-12-27 15:12:52 -0500

    Fix path to auth

--- src/app/api/checkout-sessions/route.ts
+++ src/app/api/checkout-sessions/route.ts
@@ -1,4 +1,4 @@
-import { auth } from '@/auth'
+import { auth } from '../../../../auth'
 import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 import { sql } from '@vercel/postgres'
