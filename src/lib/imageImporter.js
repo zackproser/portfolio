0950846af3d507e9063bba048f187106f2152bac
= <zackproser@gmail.com>
2024-12-29 10:09:38 -0500

    Fix build

--- src/lib/imageImporter.js
+++
@@ -1,9 +0,0 @@
-export const importLogo = async (path) => {
-  try {
-    const image = await import(`@/public${path}`);
-    return image.default;
-  } catch (error) {
-    console.error(`Error importing image: ${path}`, error);
-    return null;
-  }
-};
