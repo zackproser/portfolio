d82bbbb760ebc7ba2ee64cc08f11f63277163d19
= <zackproser@gmail.com>
2025-03-17 09:59:14 -0400

    Instrument vector db tool with email capture

--- src/components/NewsletterWrapper.tsx
+++ src/components/NewsletterWrapper.tsx
@@ -4,8 +4,8 @@ import dynamic from 'next/dynamic';
 
 const DynamicNewsletter = dynamic(() => import('./Newsletter'), { ssr: false });
 
-const NewsletterWrapper = ({ title, body }: { title: string, body: string }) => {
-  return <DynamicNewsletter title={title} body={body} />;
+const NewsletterWrapper = ({ title, body, successMessage }: { title: string, body: string, successMessage?: string }) => {
+  return <DynamicNewsletter title={title} body={body} successMessage={successMessage} />;
 };
 
 export default NewsletterWrapper;
