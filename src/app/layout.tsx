9735a14603da5baaf5496172a3ca2c2392ebd6e5
= <zackproser@gmail.com>
2025-04-28 12:59:29 -0400

    Fix gtag duplication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`h-full antialiased ${notoSans.variable} ${pressStart2P.variable}`} suppressHydrationWarning>
-      <GoogleTagManager gtmId="GTM-5MLM6LJX" />
       <head>
         <link
           rel="alternate"
