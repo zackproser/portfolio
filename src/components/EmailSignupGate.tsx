c59fd7c0208b78bc7f39fe27539f99edd0549a85
Zack Proser <zackproser@gmail.com>
2025-05-17 20:21:38 -0400

    fix email signup gate

--- src/components/EmailSignupGate.tsx
+++ src/components/EmailSignupGate.tsx
@@ -12,12 +12,13 @@ export default function EmailSignupGate({
   body
 }: EmailSignupGateProps) {
   return (
-    <div className="my-8 p-8 bg-gradient-to-br from-zinc-100 to-zinc-200 dark:from-zinc-800 dark:to-zinc-900 rounded-xl shadow-xl border border-zinc-300 dark:border-zinc-700">
+    <div className="my-8 p-10 max-w-xl mx-auto text-center bg-gradient-to-br from-zinc-100 to-zinc-200 dark:from-zinc-800 dark:to-zinc-900 rounded-xl shadow-xl border border-zinc-300 dark:border-zinc-700">
       <NewsletterWrapper
-        title={header || 'Sign in & subscribe to read for free'}
-        body={body || 'Sign in to zackproser.com and subscribe to unlock this article.'}
+        title={header || 'Unlock This Free Article'}
+        body={body || 'Join my mailing list to access the rest of this article for free.'}
         successMessage="Thanks for subscribing! Refresh to view the full article."
         position="paywall"
+        disableSticky
       />
     </div>
   )
